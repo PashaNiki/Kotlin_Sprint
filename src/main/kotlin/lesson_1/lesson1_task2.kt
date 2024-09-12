d84222a67b50baa8c6dae53a89b40bafd86dd34b
pavelnikitin <pavelnikitin@>
2024-09-12 16:49:58 +0400

    KS-1-2 +workers

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val allOrders = 75
+    val acceptOrderText = "Спасибо за покупку"
+    val workers = 1999
+
+    println(allOrders)
+    println(acceptOrderText)
+   // println(workers)   // один уволился
+    println(workers)
+}
