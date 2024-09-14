a6eda677934300c35012ec16ab03482d7d6c0413
pavelnikitin <pavelnikitin@>
2024-09-14 15:53:12 +0400

    KS-1-2 fixed output

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,10 +3,14 @@ package org.example.lesson_1
 fun main() {
     val allOrders = 75
     val acceptOrderText = "Спасибо за покупку"
-    val workers = 1999
+    var workers = 2000
+
 
     println(allOrders)
     println(acceptOrderText)
-   // println(workers)   // один уволился
+//  println(workers)
+
+    workers -= 1
     println(workers)
+
 }
