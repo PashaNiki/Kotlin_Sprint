524e5c4d3f0dd1b3b180998381cb41d9996007f2
pavelnikitin <pavelnikitin@>
2024-09-15 20:51:49 +0400

    KS-1-1 fixed space

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_1
 
 fun main() {
-    val allOrders : Int = 75
-    val acceptOrderText : String = "Спасибо за покупку!"
+    val allOrders: Int = 75
+    val acceptOrderText: String = "Спасибо за покупку!"
 
     println(allOrders)
     println(acceptOrderText)
