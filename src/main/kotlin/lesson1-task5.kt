fb32e8d2b47ddce1c093650f7f2413a24c11272d
pavelnikitin <pavelnikitin@>
2024-09-24 21:38:41 +0400

    KS-1-5 fix

--- src/main/kotlin/lesson1-task5.kt
+++ src/main/kotlin/lesson1-task5.kt
@@ -2,13 +2,14 @@ package org.example
 
 fun main() {
 
-    val secondsInSpace = 6480
+    var secondsInSpace = 6480
 
-    val hours = secondsInSpace / 3600
+    var Seconds_In_Minute = 60
+    var In_Hour = Seconds_In_Minute * 60
 
-    val minutes = (secondsInSpace % 3600) / 60
-
-    val seconds = secondsInSpace % 60
+    var hours = secondsInSpace / In_Hour
+    var minutes = (secondsInSpace % In_Hour) / Seconds_In_Minute
+    var seconds = secondsInSpace % Seconds_In_Minute
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
