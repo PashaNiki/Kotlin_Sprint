30300187614185d60abb45dc236ad81f517d1e30
pavelnikitin <pavelnikitin@>
2024-09-26 08:39:25 +0400

    KS-2-3 time_fix+const

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -1,16 +1,20 @@
 package org.example
 
-fun main(){
-    var minutesInHour = 60
-    var departureHour = 9
-    var departureMinute = 39
-    var timeInWay = 475
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
 
-    var departureMinutes = departureHour * minutesInHour + departureMinute
-    var allMinitesInTrain = departureMinutes + timeInWay
+fun main() {
 
-    var arivalHour = departureMinutes % minutesInHour
-    var arivalMinute = allMinitesInTrain % minutesInHour
-    println(String.format("%02d:%02d", arivalHour,arivalMinute))
+    val departureHour = 9
+    val departureMinute = 39
+    val timeInWay = 475
+
+    val departureMinutes = departureHour * MINUTES_IN_HOUR + departureMinute
+    val allMinitesInTrain = departureMinutes + timeInWay
+
+    val arivalHour = (allMinitesInTrain / MINUTES_IN_HOUR) % HOURS_IN_DAY
+    val arivalMinute = allMinitesInTrain % MINUTES_IN_HOUR
+
+    println(String.format("%02d:%02d", arivalHour, arivalMinute))
 }
 
