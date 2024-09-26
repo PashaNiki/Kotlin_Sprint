3926ec2ed73763e46c4d11bd80ed426cade41e03
pavelnikitin <pavelnikitin@>
2024-09-26 08:39:25 +0400

    KS-2-3 time_train_fix

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -2,6 +2,7 @@ package org.example
 
 fun main(){
     var minutesInHour = 60
+    var hoursInDay = 24
     var departureHour = 9
     var departureMinute = 39
     var timeInWay = 475
@@ -9,8 +10,8 @@ fun main(){
     var departureMinutes = departureHour * minutesInHour + departureMinute
     var allMinitesInTrain = departureMinutes + timeInWay
 
-    var arivalHour = departureMinutes % minutesInHour
-    var arivalMinute = allMinitesInTrain % minutesInHour
+    var arivalHour = (departureMinutes / minutesInHour) % hoursInDay
+    var arivalMinute = allMinitesInTrain / minutesInHour
     println(String.format("%02d:%02d", arivalHour,arivalMinute))
 }
 
