9ab904d29e6f0932a0b5b0751464bbc4a3fc1fa1
pavelnikitin <pavelnikitin@>
2024-10-09 09:32:58 +0400

    KS-2-4 buff_fix

---
+++ src/main/kotlin/lesson2-task4.kt
@@ -0,0 +1,16 @@
+package org.example
+
+const val PERCENTAGE_BUFF_DIVIDER = 100
+
+fun main() {
+
+    val crystal = 7
+    val iron = 11
+    val buff = 20
+
+    val bonusCrystal = (crystal * buff) / PERCENTAGE_BUFF_DIVIDER
+    val bonusIron = (iron * buff) / PERCENTAGE_BUFF_DIVIDER
+
+    println("Бонусная кристаллическая руда: $bonusCrystal")
+    println("Бонусная железная руда: $bonusIron" )
+}
