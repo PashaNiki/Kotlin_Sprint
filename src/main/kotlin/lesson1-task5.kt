19f8fe81bd86e25769088aa19a615dba989ab92f
pavelnikitin <pavelnikitin@>
2024-10-14 23:07:28 +0400

    KS-1-5-in_space_fix4

--- src/main/kotlin/lesson1-task5.kt
+++ src/main/kotlin/lesson1-task5.kt
@@ -6,10 +6,10 @@ fun main() {
 
     val secondsInSpace = 6480
 
-    val In_Hour = SECONDS_IN_MINUTE * SECONDS_IN_MINUTE
+    val inHour = SECONDS_IN_MINUTE * SECONDS_IN_MINUTE
 
-    val hours = secondsInSpace / In_Hour
-    val minutes = (secondsInSpace % In_Hour) / SECONDS_IN_MINUTE
+    val hours = secondsInSpace / inHour
+    val minutes = (secondsInSpace % inHour) / SECONDS_IN_MINUTE
     val seconds = secondsInSpace % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
