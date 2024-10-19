430022b8f5e676689e64f789dc8d25ea1a44feae
pavelnikitin <pavelnikitin@>
2024-10-19 13:28:17 +0400

    KS-4-4-training

--- src/main/kotlin/lesson4-task4.kt
+++ src/main/kotlin/lesson4-task4.kt
@@ -5,15 +5,13 @@ fun main () {
     val trainingDay = 5
 
     val armsDay = trainingDay % 2 !=0
-    val legsDay = !armsDay
-    val backDay = legsDay
-    val absDay = armsDay
+
 
     println("""
     Упражнения для рук: $armsDay     
-    Упражнения для ног: $legsDay      
-    Упражнения для спины: $backDay 
-    Упражнения для пресса: $absDay
+    Упражнения для ног: ${!armsDay}      
+    Упражнения для спины: ${!armsDay} 
+    Упражнения для пресса: $armsDay
  """.trimIndent()
 )
 
