3afaf7c790b71b986ba63f4fc8b9169b7a9ea392
pavelnikitin <pavelnikitin@>
2024-10-27 14:03:37 +0400

    KS-6-4-game

---
+++ src/main/kotlin/lesson6-task4.kt
@@ -0,0 +1,25 @@
+package org.example
+
+import kotlin.random.Random
+
+fun main() {
+
+    val Number = Random.nextInt(1,10)
+    var attempts = 5
+
+    println("Угадайте число от 1 до 9. У вас есть $attempts попыток")
+
+    while (attempts > 0) {
+        println("Введите ваше число:")
+        val userNumber = readln().toInt()
+
+        if(userNumber == Number) {
+            println("Это была великолепная игра!")
+            return
+        } else {
+         attempts--
+            println("Неверно. Осталось попыток $attempts")
+        }
+    }
+    println("Было загадано число $attempts")
+}
