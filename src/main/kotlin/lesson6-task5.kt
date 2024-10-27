32d5afd04e5f4e947ce726a1f95c4baf9545fbb9
pavelnikitin <pavelnikitin@>
2024-10-27 15:28:38 +0400

    KS-6-5-regV2

---
+++ src/main/kotlin/lesson6-task5.kt
@@ -0,0 +1,52 @@
+package org.example
+
+import kotlin.random.Random
+
+fun main() {
+
+    println("Создайте логин:")
+    val createLongin = readln()
+
+    println("Создайте пароль:")
+    val createPassword = readln()
+
+    var login: String
+    var password: String
+
+    do {
+        println("Введите логин:")
+        login = readln()
+
+        println("Введите пароль")
+        password = readln()
+
+        if (login != createLongin || password != createPassword) {
+            println("Неверный логин или пароль, попробуйде еще раз")
+        }
+
+    }    while (login != createLongin || password != createPassword)
+
+    var attempts = 3
+    var authorized = false
+
+    while (attempts > 0 && !authorized) {
+        val number1 = Random.nextInt(1,10)
+        val number2 = Random.nextInt(1,10)
+        val correctAnswer = number1 + number2
+
+        println("Для входа в систему, решите пример: $number1 + $number2")
+        val userAnswer = readln().toIntOrNull()
+
+        if(userAnswer == correctAnswer) {
+            println("Добро пожаловать")
+            authorized = true
+        } else {
+            attempts--
+            if (attempts > 0) {
+                println("Неверный ответ. У вас осталость $attempts попыток")
+            } else {
+                println("Доступ запрещен")
+            }
+        }
+    }
+}
