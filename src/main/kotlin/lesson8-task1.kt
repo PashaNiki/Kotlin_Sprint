c61aa3b44bea66207cf142e4736375213d7d26a4
pavelnikitin <pavelnikitin@>
2024-11-01 20:46:31 +0400

    KS-8-1-advertising

---
+++ src/main/kotlin/lesson8-task1.kt
@@ -0,0 +1,11 @@
+package org.example
+
+fun main() {
+
+    val viewsAtWeek = arrayOf(100, 120, 150, 180, 190, 200, 210)
+
+    val totalView = viewsAtWeek.sum()
+
+    println("Всего просмотров за неделю: $totalView")
+
+}
