637c8504d3c1d8f1ba48322939c2e42af66d80ac
pavelnikitin <pavelnikitin@>
2024-11-07 22:09:12 +0400

    KS-9-5-five_ing

---
+++ src/main/kotlin/lesson9-task5.kt
@@ -0,0 +1,20 @@
+package org.example
+
+fun main() {
+
+    val ingredients = mutableSetOf<String>()
+
+    repeat(5) {
+        println("Введите название ингридиента:")
+        val input = readln().trim()
+        ingredients.add(input)
+    }
+
+    val sortIngredients = ingredients.toList().sorted()
+
+    val formattedIngredients = sortIngredients.mapIndexed { index, ingredient ->
+        if (index == 0) ingredient.replaceFirstChar { it.uppercase() } else ingredient
+    }
+    println("Ингредиенты в алфавитном порядке:")
+    println(formattedIngredients.joinToString (", "))
+}
