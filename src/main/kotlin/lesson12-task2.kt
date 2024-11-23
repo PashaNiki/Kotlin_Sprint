0ca5f0a1ef42e0b42fdd00bd826d8b86f6e7d8e1
pavelnikitin <pavelnikitin@>
2024-11-23 09:37:43 +0400

    KS-12-2-WeatherV2

--- src/main/kotlin/lesson12-task2.kt
+++ src/main/kotlin/lesson12-task2.kt
@@ -3,11 +3,10 @@ package org.example
 const val DEGREE = "°C"
 
 class WeatherData(
-
     val dayTemperature: Int,
     val nightTemperature: Int,
     val hasPrecipitation: Boolean
-    )  {
+) {
 
     fun printWeatherInfo() {
         val pricipitationInfo = if (hasPrecipitation) "Осадки: есть" else "Осадки: нет"
@@ -19,9 +18,9 @@ class WeatherData(
 
 fun main() {
 
-    val weatherToday = WeatherData (
+    val weatherToday = WeatherData(
         dayTemperature = 28,
-        nightTemperature =18,
+        nightTemperature = 18,
         hasPrecipitation = false
     )
 
