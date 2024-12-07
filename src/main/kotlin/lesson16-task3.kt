7766dec75e025841129533437470f032a7b064dc
pavelnikitin <pavelnikitin@>
2024-12-07 20:27:31 +0400

    KS-16-3-pass

---
+++ src/main/kotlin/lesson16-task3.kt
@@ -0,0 +1,22 @@
+package org.example
+
+class User(private val login: String, private val password: String) {
+
+    fun validatePassword(inputPassword: String): Boolean {
+        return inputPassword == password
+    }
+
+    fun getLogin(): String {
+        return login
+    }
+}
+
+fun main() {
+    val user = User("user123", "securePassword")
+
+    val isPasswordCorrect = user.validatePassword("securePassword")
+    println("Пароль верный: $isPasswordCorrect")
+
+    val isPasswordInCorrect = user.validatePassword("wrongPassword")
+    println("Пароль верный: $isPasswordCorrect")
+}
