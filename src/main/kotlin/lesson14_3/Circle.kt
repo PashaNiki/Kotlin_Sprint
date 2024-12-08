abcf02d8148971fe712411db6e8157e6b973c65d
PashaNiki <52444612+PashaNiki@users.noreply.github.com>
2024-12-08 14:48:36 +0400

    Delete src/main/kotlin/lesson14_3/Circle.kt

--- src/main/kotlin/lesson14_3/Circle.kt
+++
@@ -1,9 +0,0 @@
-package org.example.lesson14_3
-
-import kotlin.math.PI
-
-class Circle(color: String, val radiues: Double) : Figure(color) {
-    override fun calculateArea(): Double = PI * radiues * radiues
-
-    override fun calculatePerimeter(): Double = 2 * PI * radiues
-}
