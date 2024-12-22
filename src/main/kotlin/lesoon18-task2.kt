82218e0e8b80f41edbaa0cf3d937010fad147883
pavelnikitin <pavelnikitin@>
2024-12-22 16:25:36 +0400

    KS-18-2-bones_fix

--- src/main/kotlin/lesoon18-task2.kt
+++ src/main/kotlin/lesoon18-task2.kt
@@ -2,21 +2,33 @@ package org.example
 
 import kotlin.random.Random
 
-open class Dice(
-    private val sides: Int
-) {
+open class Dice {
+   protected open var sides: Int = 0
+
     open fun roll() {
         val result = Random.nextInt(1, sides +1)
         println("Бросок кости с $sides гранями: $result")
     }
 }
 
-class D4: Dice(4)
-class D6: Dice(6)
-class D8: Dice(8)
+class FourSidedDice: Dice() {
+    override var sides: Int = 4
+}
+
+class SixSidedDice: Dice() {
+    override var sides: Int = 6
+}
+
+class EightSidedDice: Dice() {
+    override var sides: Int = 8
+}
 
  fun main() {
-    val dicesList: List<Dice> = listOf(D4(), D6(), D8())
+    val dicesList: List<Dice> = listOf(
+        FourSidedDice(),
+        SixSidedDice(),
+        EightSidedDice()
+     )
 
      for (dice in dicesList){
          dice.roll()
