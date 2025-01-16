2e8b484db5d1415e6a739025b1267ac13ee57cbb
pavelnikitin <pavelnikitin@>
2025-01-16 14:51:27 +0400

    KS-19-aquarium

---
+++ src/main/kotlin/lesson19-task1.kt
@@ -0,0 +1,19 @@
+package org.example
+
+enum class FishType {
+    GUPPY,
+    ANGELFISH,
+    GOLDFISH,
+    SIAMESE_FIGHTING_FISH
+}
+
+fun showAvialableFish() {
+    println("Доступные рыбы для добавления в ваш аквариум:")
+    for (fish in FishType.values()) {
+        println("- ${fish.name.lowercase().replace('_', ' ')}")
+    }
+}
+
+fun main() {
+    showAvialableFish()
+}
