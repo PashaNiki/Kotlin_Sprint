3af946f7d4ce5f9fd98a3c0f3bbfe2752c2655ee
pavelnikitin <pavelnikitin@>
2025-01-28 22:41:24 +0400

    KS-20-3-keyFix

--- src/main/kotlin/lesson20-task3.kt
+++ src/main/kotlin/lesson20-task3.kt
@@ -5,18 +5,15 @@ class Player(
     var hasKey: Boolean
 )
 
-val chechKey: (Player) -> String = { player: Player ->
-    if (player.hasKey) "Игрок открыл дверь" else "Дверь заперта"
-}
-
 fun main() {
-
-    val playerWithKey = Player(name = "Женя", hasKey = true)
-    val playerWithoutKey = Player(name = "Катя", hasKey = false)
 
     val checkKey: (Player) -> String = { player: Player ->
         if (player.hasKey) "Игрок открыл дверь" else "Дверь заперта"
     }
-    println("${playerWithoutKey.name}: ${checkKey(playerWithKey)}")
-    println("${playerWithoutKey.name}: ${checkKey(playerWithoutKey)}")
-}
+
+        val playerWithKey = Player(name = "Женя", hasKey = true)
+        val playerWithoutKey = Player(name = "Катя", hasKey = false)
+
+        println("${playerWithoutKey.name}: ${checkKey(playerWithKey)}")
+        println("${playerWithoutKey.name}: ${checkKey(playerWithoutKey)}")
+    }
