a23b45c4ac9c4df943028fc51d82af0730179204
pavelnikitin <pavelnikitin@>
2025-02-01 14:55:43 +0400

    KS-21-4-fileV2

--- src/main/kotlin/lesson21-task4.kt
+++ src/main/kotlin/lesson21-task4.kt
@@ -2,11 +2,13 @@ package org.example
 
 import java.io.File
 
+
 fun File.prependWord(word: String) {
     val existingText = if (this.exists()) this.readText() else ""
     val newText = word.lowercase() + "\n" + existingText
     this.writeText(newText)
 }
+
 fun main() {
     val file = File("output.txt")
 
